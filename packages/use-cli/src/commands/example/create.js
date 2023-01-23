00d7d236474cfdfc7b0e929b0876017bb582af41
RIX <94552463+rixdotworks@users.noreply.github.com>
2023-01-23 23:24:53 +0100

    fix: remove example use-cli command

--- packages/use-cli/src/commands/example/create.js
+++
@@ -1,9 +0,0 @@
-module.exports = (program) => {
-    const createService = program.command('create').argument('workerName').description('Create a new service worker')
-    
-    createService.action((workerName) => {
-        console.log(`--> creating service worker: ${workerName}`)
-    })
-
-    return createService
-}
--- packages/use-cli/src/commands/example/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = (program) => {
-    const services = program.command('services')
-
-    require('./list')(services)
-    require('./create')(services)
-
-    return services
-}
